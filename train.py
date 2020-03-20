93be78dfd172684edf0c91d43e9a0e77f7552326
RourouX18 <48292449+RourouX18@users.noreply.github.com>
2020-03-20 12:13:52 +0100

    Remove print

--- train.py
+++ train.py
@@ -1,8 +1,6 @@
 import gym
 import numpy as np
 
-print("data")
-
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import DummyVecEnv, SubprocVecEnv, VecNormalize
 from stable_baselines import PPO2
